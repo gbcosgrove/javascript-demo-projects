95996fe5e60dbc358c96d34e1fd373a75c73a4f0
Greg Cosgrove <gregcosgrove@gmail.com>
2014-11-18 12:20:24 -0500

    Add fancybox and style.  App complete

--- youtube-search-engine/js/script.js
+++ youtube-search-engine/js/script.js
@@ -27,4 +27,180 @@ $(function() {
     }
   });
 
+  $('#search-form').submit(function(e) {
+    e.preventDefault();
+  });
+
 })
+
+// Youtube search function
+
+function search() {
+  // Clear out results
+  $('#results').html('');
+  $('#buttons').html('');
+
+  //Get form input
+  q = $('#query').val();
+
+  // Run Get Request
+  $.get(
+      "https://www.googleapis.com/youtube/v3/search",{
+        part: 'snippet, id',
+        q: q,
+        type: 'video',
+        key: 'AIzaSyCp3MPfPooUgQtug3NzzpH8Cb36Fe0HYdk'},
+        function(data) {
+          var nextPageToken = data.nextPageToken;
+          var prevPageToken = data.prevPageToken;
+
+          console.log(data);
+
+          $.each(data.items, function(i, item){
+            // Get Output
+            var output = getOutput(item);
+
+            // Display Results
+            $('#results').append(output);
+          });
+
+          var buttons = getButtons(prevPageToken, nextPageToken);
+
+          // Display Buttons
+          $('#buttons').append(buttons);
+
+        }
+    );
+}
+
+// Next Page Function
+function nextPage() {
+  var token = $('#next-button').data('token');
+  var q = $('#next-button').data('query');
+   // Clear out results
+  $('#results').html('');
+  $('#buttons').html('');
+
+  //Get form input
+  q = $('#query').val();
+
+  // Run Get Request
+  $.get(
+      "https://www.googleapis.com/youtube/v3/search",{
+        part: 'snippet, id',
+        q: q,
+        pageToken: token,
+        type: 'video',
+        key: 'AIzaSyCp3MPfPooUgQtug3NzzpH8Cb36Fe0HYdk'},
+        function(data) {
+          var nextPageToken = data.nextPageToken;
+          var prevPageToken = data.prevPageToken;
+
+          console.log(data);
+
+          $.each(data.items, function(i, item){
+            // Get Output
+            var output = getOutput(item);
+
+            // Display Results
+            $('#results').append(output);
+          });
+
+          var buttons = getButtons(prevPageToken, nextPageToken);
+
+          // Display Buttons
+          $('#buttons').append(buttons);
+
+        }
+    );
+
+}
+
+// Prev Page Function
+function prevPage() {
+  var token = $('#prev-button').data('token');
+  var q = $('#prev-button').data('query');
+   // Clear out results
+  $('#results').html('');
+  $('#buttons').html('');
+
+  //Get form input
+  q = $('#query').val();
+
+  // Run Get Request
+  $.get(
+      "https://www.googleapis.com/youtube/v3/search",{
+        part: 'snippet, id',
+        q: q,
+        pageToken: token,
+        type: 'video',
+        key: 'AIzaSyCp3MPfPooUgQtug3NzzpH8Cb36Fe0HYdk'},
+        function(data) {
+          var nextPageToken = data.nextPageToken;
+          var prevPageToken = data.prevPageToken;
+
+          console.log(data);
+
+          $.each(data.items, function(i, item){
+            // Get Output
+            var output = getOutput(item);
+
+            // Display Results
+            $('#results').append(output);
+          });
+
+          var buttons = getButtons(prevPageToken, nextPageToken);
+
+          // Display Buttons
+          $('#buttons').append(buttons);
+
+        }
+    );
+
+}
+
+// Build Output
+
+function getOutput(item) {
+  var videoId = item.id.videoId;
+  var title = item.snippet.title;
+  var description = item.snippet.description;
+  var thumb = item.snippet.thumbnails.high.url;
+  var channelTitle = item.snippet.channelTitle;
+  var videoDate = item.snippet.publishedAt;
+
+  // Build Output String
+
+  var output = '<li>' +
+  '<div class="list-left">'+
+  '<img src="'+thumb+'">'+
+  '</div>'+
+  '<div class="list-right">'+
+  '<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>'+
+  '<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>'+
+  '<p>'+description+'</p>'+
+  '</div>'+
+  '</li>'+
+  '<div class="clearfix"></div>'+
+  '';
+
+  return output;
+
+}
+
+// Build the Buttons
+function getButtons(prevPageToken, nextPageToken) {
+  if(!prevPageToken){
+    var btnoutput = '<div class="button-container">'+
+    '<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"'
+    + 'onclick="nextPage();">Next Page</button></div>';
+  } else {
+    var btnoutput = '<div class="button-container">'+
+    '<button id="next-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"'
+    + 'onclick="prevPage();">Prev Page</button>' +
+    '<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"'
+    + 'onclick="nextPage();">Next Page</button></div>';
+  }
+
+  return btnoutput;
+}
